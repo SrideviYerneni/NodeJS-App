fed37db00c4693a5e67eb7cea4853581990d9eca
Sridevi yerneni <nxy17640@ucmo.edu>
2017-11-02 17:30:12 -0500

    Update center.jQuery.js

--- public/center.jQuery.js
+++ public/center.jQuery.js
@@ -1,23 +1,22 @@
 jQuery.fn.center = function(){
-    var element = this;
-    $(element).load(function(){
-          changeCss();
+  var element = this;
+  $(element).load(function(){
   
-    $(window).bind("resize",function(){
+  changeCss();
+  $(window).bind("resize",function(){
       changeCss();
-     });
+   });
   function changeCss(){
-      
     var imageHeight = $(element).height();
     var imageWidth = $(element).width();
     var windowWidth = $(window).width();
     var windowHeight = $(window).height();
-  
-    $(element).css({
-      "position" :"absolute",
-      "left" : windowWidth / 2 - imageWidth / 2,
-      "top" : windowHeight / 2 - imageHeight / 2
+    
+     $(element).css({
+       "position" :"absolute",
+       "left" : windowWidth / 2 - imageWidth / 2,
+       "top" : windowHeight / 2 - imageHeight / 2
       });
-     };
-   });
+   };
+  });
   };
